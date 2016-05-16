1d561ba651ffb2a07071782b215da1e09de2ed67
Kiran Kumawat <kiran.kumawat@metacube.com>
2016-05-16 14:49:13 +0530

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+items = [{item_name:'Chai',price:7},
+		{item_name:'Kachori',price:15},
+		{item_name:'Fried Idli',price:25},
+		{item_name:'Chips',price:10},
+		{item_name:'Samosa',price:10},
+		{item_name:'Bread Bada',price:10},
+		{item_name:'Bread Pakoda',price:20},
+		{item_name:'Mirchi Bada',price:10}
+	]
+	items.each do |item|
+		exist_item = Item.find_by(item_name:item[:item_name])
+		Item.create(item) if exist_item.blank?
+	end

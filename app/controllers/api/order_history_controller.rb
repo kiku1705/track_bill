6aa7613e85ca785c9d491cb59d6472093da11133
Kiran Kumawat <kiran.kumawat@metacube.com>
2016-05-16 15:06:24 +0530

    error changes

--- app/controllers/api/order_history_controller.rb
+++ app/controllers/api/order_history_controller.rb
@@ -2,12 +2,12 @@ class Api::OrderHistoryController < Api::BaseController
 
 	def order_histories
 		if params[:order_histories]
-		    cnt = 0
-		    params[:order_histories].try(:each) do |order_attr|
-		    	order_history = OrderHistory.create(order_attr)
-		    	cnt++ if order_history.persisted?
-		    end
-		    render json: { count: cnt }, status: 200
+	    cnt = 0
+	    params[:order_histories].try(:each) do |order_attr|
+	    	order_history = OrderHistory.create(order_attr)
+	    	cnt += 1 if order_history.persisted?
+	    end
+	    render json: { count: cnt }, status: 200
 		else
 			render json: { }, status: :unproccesable_entity
 		end

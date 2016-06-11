4966de20173af74a953a851ea95fc7fff8b9488b
Kiran Kumawat <kiran.kumawat@metacube.com>
2016-06-11 17:35:32 +0530

    error resolve

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,5 @@
 class Order < ActiveRecord::Base
-	attr_accessible \		
-		:item_price_id,
-		:order_time,
-		:user_id
+	attr_accessible :item_price_id, :order_time, :user_id
 
 	belongs_to :item_price
 	belongs_to :user
